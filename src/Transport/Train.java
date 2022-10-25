4a159c089d60680fc2e15be260d2a84ae8c5d195
Admin <anstdor@gmail.com>
2022-10-25 15:21:44 +0300

    changes3

--- src/Transport/Train.java
+++ src/Transport/Train.java
@@ -1,23 +1,77 @@
 package Transport;
 
-public class Train extends Transport {
+public abstract class Train extends Transport {
+
+    private int priceOfTrip;
+    private int timeOfTrip;
+    private String stationName;
+    private String finalStop;
     private int numberOfWagons;
 
-    public Train(String brand, String model, int productionYear, String productionCountry, int maxSpeed, int priceOfTrip, int timeOfTrip, String stationName, String finalStop, int numberOfWagons) {
-        super(brand, model, productionYear, productionCountry, null, 0, "Топливо");
+    public Train(String brand, String model, int productionYear, String productionCountry, String color, int maxSpeed, int priceOfTrip, int timeOfTrip, String stationName, String finalStop, int numberOfWagons) {
+        super(brand, model, productionYear, productionCountry, null, 0,"Топливо");
         setMaxSpeed(maxSpeed);
         if (priceOfTrip == 0) {
             this.priceOfTrip = 0000;
-
-            public int getNumberOfWagons () {
-                public void setNumberOfWagons ( int numberOfWagons){
-                    this.numberOfWagons = numberOfWagons;
-                }
-
-                @Override
-                public void refill () {
-
-                }
-            }
+        } else {
+            this.priceOfTrip = priceOfTrip;
+        }
+        if (timeOfTrip == 0) {
+            this.timeOfTrip = 12;
+        } else {
+            this.timeOfTrip = timeOfTrip;
+        }
+        if (stationName == null) {
+            this.stationName = "Такой станции не существует";
+        } else {
+            this.stationName = stationName;
+        }
+        if (finalStop == null) {
+            this.finalStop = "Такой конечной станции не существует";
+        } else {
+            this.finalStop = finalStop;
+        }
+        if (numberOfWagons == 0) {
+            this.numberOfWagons = 1000;
+        } else {
+            this.numberOfWagons = numberOfWagons;
         }
     }
+
+    public int getPriceOfTrip() {
+        return priceOfTrip;
+    }
+
+    public int getTimeOfTrip() {
+        return timeOfTrip;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public String getFinalStop() {
+        return finalStop;
+    }
+
+    public void setFinalStop(String finalStop) {
+        this.finalStop = finalStop;
+    }
+
+    public int getNumberOfWagons() {
+        return numberOfWagons;
+    }
+
+    public void setNumberOfWagons(int numberOfWagons) {
+        this.numberOfWagons = numberOfWagons;
+    }
+
+    @Override
+    public void refill() {
+
+    }
+}
