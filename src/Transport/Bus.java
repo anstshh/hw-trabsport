339721cb6b0d71cd426d7c98e01b5746a7a66493
Admin <anstdor@gmail.com>
2022-10-25 15:06:41 +0300

    changes2

--- src/Transport/Bus.java
+++ src/Transport/Bus.java
@@ -2,15 +2,39 @@ package Transport;
 
 public class Bus  extends Transport{
 
-    public Bus(String brand, String model, int productionYear, String productionCountry) {
-        super(brand, model, productionYear, productionCountry,"Топливо");
+    public String destination;
+    private String statusRefill;
+
+    public String getDestination() {
+        return destination;
     }
 
-    public Bus(String brand, String model, int productionYear, String productionCountry, String color, int maxSpeed) {
-        super(brand, model, productionYear, productionCountry, color, maxSpeed,"Топливо");
+    public void setDestination(String destination) {
+        if (destination != null) {
+            this.destination = destination;
+        } else {
+            this.destination = "место назначения не указано";
+        }
+    }
+
+    public Bus(String brand, String model, int productionYear, String productionCountry, String color, int maxSpeed, String destination ) {
+        super(brand, model, productionYear, productionCountry, color, maxSpeed);
     }
 
     @Override
-    public void refill() {
+    protected void refill (Boolean gasoline, Boolean diesel, Boolean electricity) {
+        if (gasoline || diesel) {
+            statusRefill = "заправлен";
+        } else {
+            statusRefill = "не заправлен";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Bus{" +
+                "destination='" + destination + '\'' +
+                ", statusRefill='" + statusRefill + '\'' +
+                '}';
     }
 }
